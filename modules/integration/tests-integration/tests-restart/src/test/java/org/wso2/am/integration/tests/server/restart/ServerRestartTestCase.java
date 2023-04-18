3b121ecda1fa6c14e635ab5959b144b4232f7ee2
Thisal Tennakoon <thisaltennakoon@gmail.com>
2023-04-18 22:04:26 +0530

    Revert "Merge branch 'master' of https://github.com/thisaltennakoon/product-apim"
    This reverts commit 64174ad423a3c54c90212f7e35b8d3e75c658dd0, reversing
    changes made to 05939d50b6b3371e527ce89673895b29ca431f81.

--- modules/integration/tests-integration/tests-restart/src/test/java/org/wso2/am/integration/tests/server/restart/ServerRestartTestCase.java
+++ modules/integration/tests-integration/tests-restart/src/test/java/org/wso2/am/integration/tests/server/restart/ServerRestartTestCase.java
@@ -628,7 +628,7 @@ public class ServerRestartTestCase extends APIManagerLifecycleBaseTest {
         restartServer();
     }
 
-    protected static void restartServer() throws Exception {
+    private void restartServer() throws Exception {
         AutomationContext superTenantKeyManagerContext = new AutomationContext(APIMIntegrationConstants.AM_PRODUCT_GROUP_NAME,
                 APIMIntegrationConstants.AM_KEY_MANAGER_INSTANCE,
                 TestUserMode.SUPER_TENANT_ADMIN);
